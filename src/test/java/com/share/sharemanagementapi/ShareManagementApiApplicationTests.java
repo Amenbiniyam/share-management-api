f5c0fe6521a01e7369ed2d0852b9fa256b353460
BiniyamK <biniasella@gmail.com>
2023-03-30 11:11:42 +0300

    Password Field Encrypted and Saved and Test Annotation Changed

--- src/test/java/com/share/sharemanagementapi/ShareManagementApiApplicationTests.java
+++ src/test/java/com/share/sharemanagementapi/ShareManagementApiApplicationTests.java
@@ -3,7 +3,6 @@ package com.share.sharemanagementapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ShareManagementApiApplicationTests {
 
     @Test
